73dc87648887d91882340123bb361beb09943571
Yahm3 <240418541@tut4life.ac.za>
2026-08-15 18:18:42 +0200

    fix: I had made a mistake of initializing handshake variable to nothing in the beginning of the client_handshake function whereas a handshake string must always end with \r\n

--- main.v
+++ main.v
@@ -1,23 +1,27 @@
 module main
 import net
 
-const f_text   = 0x1 // f -> frame data
-const f_binary = 0x2
+const f_text   = 0x01 // f -> frame data
+const f_binary = 0x02
+const f_close  = 0x08
+const f_ping   = 0x09
+const f_pong   = 0x0A
 const tcp_ip   = '127.0.0.1'
-const tcp_port = 5006
+const tcp_port = 80
 const buffer_size = 1024 * 1024
 
-fn main() {
-}
-
-fn client_handshake(client_socket net.Socket,host string) {
-  mut handshake := ""
-  handshake += "GET / HTTP/1.1\r\n"
-  handshake += "Host: ${host}"
+fn client_handshake(mut socket net.TcpConn, host string, port int) !net.Addr {
+  mut handshake := "GET / HTTP/1.1\r\n"
+  handshake += "Host: ${host}:${port}\r\n"
   handshake += "Upgrade: websocket\r\n"
+  handshake += 'Connection: Upgrade\r\n'
   handshake += "Sec-WebSocket-Key: dGhlIHNhbXBsZSBub25jZQ==\r\n"
   handshake += "Sec-WebSocket-Version: 13\r\n"
   handshake += "\r\n"
 
-  client_socket.address(handshake.)
+  socket.write_string(handshake)!
+  return socket.addr()!
+}
+
+fn main() {
 }
